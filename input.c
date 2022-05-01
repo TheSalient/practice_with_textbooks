9b75113fa530128c923b8c8fdd539ac63d34ed1e
TheSalient <abdulazeezlawal236@gmail.com>
2022-05-01 18:24:20 +0100

    this is input file

---
+++ input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function.
+ *
+ * Return: return int value.
+ */
+int main(void)
+{
+	int a;
+
+	a = 12;
+	printf("%d\n", a);
+	int b, c, d;
+
+	scanf("%d %d", &c, &b);
+	d = b * c;
+	printf("%d\n", d);
+	return (0);
+}
